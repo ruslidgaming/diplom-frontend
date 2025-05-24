d3a48e59581d8c58ff5483171d8b90f9d8b47033
azizbekktits <>
2025-05-24 21:48:47 +0300

    asd

--- src/modules/admin/courses/catalog.jsx
+++ src/modules/admin/courses/catalog.jsx
@@ -50,6 +50,7 @@ function Courses() {
 
             <div className="courses__items">
                 {
+                    courseCatalogList.length > 0 ??
                     courseCatalogList.map((item, index) => (
                         <div className="courses__item item-course" key={index}>
                             <div>
