eaa33c718a3e8246fb63ea91f16209db5bbe85e7
azizbekktits <>
2025-05-24 21:41:08 +0300

    sdqw

--- src/modules/admin/courses/catalog.jsx
+++ src/modules/admin/courses/catalog.jsx
@@ -41,9 +41,9 @@ function Courses() {
             </div>
 
             <div className="courses__items">
-                {courseCatalogList.map((item, index) => {
-
-                    return (
+                {
+                    courseCatalogList.length > 0 &&
+                    courseCatalogList.map((item, index) => (
                         <div className="courses__item item-course" key={index}>
                             <div>
                                 <div className="item-course__img">
@@ -58,9 +58,7 @@ function Courses() {
                             </div>
                             <Link className="item-course__link _btn" to={"/admin/courcses/show/" + item.id}>Подробнее</Link>
                         </div>
-                    )
-                }
-                )}
+                    ))}
 
                 <div className="courses__item item-course">
                     <div>
