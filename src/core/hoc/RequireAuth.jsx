4e12633d5ed1ca142a3347c7af0e37cc08f6e799
azizbekktits <>
2025-06-03 04:57:34 +0300

    asd

--- src/core/hoc/RequireAuth.jsx
+++ src/core/hoc/RequireAuth.jsx
@@ -4,10 +4,6 @@ import { useAuth } from './AuthProvider';
 export const RequireAuth = ({ role, children }) => {
     const { pathname } = useLocation();
     const { user } = useAuth();
-    user.role = "mentor"
-
-    console.log(user.role)
-    console.log(role.includes(user.role))
 
     return role.includes(user.role)
         ? children
